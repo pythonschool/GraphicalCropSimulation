7ff65afced41cb7ec631cddf5a32fc48450a8437
Adam McNicol <adamsorders@mac.com>
2013-01-15 15:21:00 +0000

    code for crop view layout
    no graphics at this stage
    
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- crop_simulation.py
+++ crop_simulation.py
@@ -46,6 +46,43 @@ class CropWindow(QMainWindow):
 		#connections
 		self.instantiate_button.clicked.connect(self.instantiate_crop)
 
+	def create_view_crop_layout(self):
+		#this is the second layout of the window - view the crop growth
+
+		self.growth_label = QLabel("Growth")
+		self.days_label = QLabel("Days Growing")
+		self.status_label = QLabel("Crop Status")
+
+		self.growth_line_edit = QLineEdit()
+		self.days_line_edit = QLineEdit()
+		self.status_line_edit = QLineEdit()
+
+		self.manual_grow_button = QPushButton("Manually Grow")
+		self.automatic_grow_button = QPushButton("Automatically Grow")
+
+		self.grow_grid = QGridLayout()
+		self.status_grid = QGridLayout()
+
+		#add label widgets to the status layout
+		self.status_grid.addWidget(self.growth_label,0,0)
+		self.status_grid.addWidget(self.days_label,1,0)
+		self.status_grid.addWidget(self.status_label,2,0)
+
+		#add line edit widgets to the status layout
+		self.status_grid.addWidget(self.growth_line_edit,0,1)
+		self.status_grid.addWidget(self.days_line_edit,1,1)
+		self.status_grid.addWidget(self.status_line_edit,2,1)
+
+		#add widgets/layouts to the grow layout
+		self.grow_grid.addLayout(self.status_grid,0,1)
+		self.grow_grid.addWidget(self.manual_grow_button,1,0)
+		self.grow_grid.addWidget(self.automatic_grow_button,1,1)
+
+		#create a widget to display the grow layout
+		self.view_crop_widget = QWidget()
+		self.view_crop_widget.setLayout(self.grow_grid)
+
+
 	def instantiate_crop(self):
 		crop_type = self.crop_radio_buttons.selected_button() #get the radio button that was selected
 		if crop_type == 1:
