57452e52d1d5f9b0c167be64b89b2361f0914475
Adam McNicol <adamsorders@mac.com>
2013-01-15 15:30:37 +0000

    the crop view class
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ crop_view_class.py
@@ -0,0 +1,33 @@
+from PyQt4.QtGui import *
+import crop_resources
+
+class CropView(QGraphicsView):
+	"""this class provides a graphics view that has the required resources for displaying status of wheat and potato crops"""
+
+	def __init__(self,crop_type):
+		super().__init__()
+		self.resources(crop_type) #create the necessary graphics scenes	
+
+	def resources(self,crop_type):
+		if crop_type == 1:
+			name = "wheat"
+		elif crop_type == 2:
+			name = "potato"
+
+		#get the graphics
+		seed = QPixmap(":/{0}_seed.png".format(name))
+		seedling = QPixmap(":/{0}_seedling.png".format(name))
+		young = QPixmap(":/{0}_young.png".format(name))
+		mature = QPixmap(":/{0}_mature.png".format(name))
+		old = QPixmap(":/{0}_old.png".format(name))
+		crop_pictures = [seed, seedling, young, mature, old]
+
+		#add the graphics to scenes
+		self.crop_scenes = []
+		for each in crop_pictures:
+			self.crop_scenes.append(QGraphicsScene())
+			self.crop_scenes[-1].addPixmap(each)
+		self.setScene(self.crop_scenes[0]) #set the initial scene
+
+	def switch_scene(self,scene):
+		self.setScene(self.crop_scenes[scene])
