fd101da602287df97cf496ad36d8a663f8a7561f
Adam McNicol <adamsorders@mac.com>
2013-01-17 15:02:41 +0000

    changed dialog boxes to spin box rather than line edits
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- manual_grow_dialog_class.py
+++ manual_grow_dialog_class.py
@@ -6,19 +6,23 @@ class ManualGrowDialog(QDialog):
 	def __init__(self):
 		super().__init__()
 
-		self.water_line_edit = QLineEdit()
-		self.light_line_edit = QLineEdit()
+		self.water_spinbox = QSpinBox()
+		self.light_spinbox = QSpinBox()
 
-		self.water_line_edit.setPlaceholderText("Water Value")
-		self.light_line_edit.setPlaceholderText("Light Value")
+		self.water_spinbox.setRange(0,10)
+		self.light_spinbox.setRange(0,10)
 
-		self.water_line_edit.clearFocus()
+		self.water_spinbox.setSuffix(" Water")
+		self.light_spinbox.setSuffix(" Light")
+
+		self.water_spinbox.setValue(1)
+		self.light_spinbox.setValue(1)
 
 		self.submit_button = QPushButton("Enter Values")
 
 		self.dialog_layout = QVBoxLayout()
-		self.dialog_layout.addWidget(self.light_line_edit)
-		self.dialog_layout.addWidget(self.water_line_edit)
+		self.dialog_layout.addWidget(self.light_spinbox)
+		self.dialog_layout.addWidget(self.water_spinbox)
 		self.dialog_layout.addWidget(self.submit_button)
 
 		self.setLayout(self.dialog_layout)
@@ -27,4 +31,4 @@ class ManualGrowDialog(QDialog):
 		self.submit_button.clicked.connect(self.close)
 
 	def values(self):
-		return int(self.light_line_edit.text()), int(self.water_line_edit.text())
+		return int(self.light_spinbox.value()), int(self.water_spinbox.value())
