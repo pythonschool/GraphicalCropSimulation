f1ce05e4d31ef343368f4f74b4d425416547f637
Adam McNicol <adammcnicol@mac.com>
2013-02-24 11:31:49 +0000

    convert indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- crop_view_class.py
+++ crop_view_class.py
@@ -2,29 +2,29 @@ from PyQt4.QtGui import *
 import crop_resources
 
 class CropView(QGraphicsView):
-	"""this class provides a graphics view that has the required resources for displaying status of wheat and potato crops"""
+    """this class provides a graphics view that has the required resources for displaying status of wheat and potato crops"""
 
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-	def resources(self,name):
-		#get the graphics
-		seed = QPixmap(":/{0}_seed.png".format(name))
-		seedling = QPixmap(":/{0}_seedling.png".format(name))
-		young = QPixmap(":/{0}_young.png".format(name))
-		mature = QPixmap(":/{0}_mature.png".format(name))
-		old = QPixmap(":/{0}_old.png".format(name))
-		crop_pictures = [seed, seedling, young, mature, old]
+    def resources(self,name):
+        #get the graphics
+        seed = QPixmap(":/{0}_seed.png".format(name))
+        seedling = QPixmap(":/{0}_seedling.png".format(name))
+        young = QPixmap(":/{0}_young.png".format(name))
+        mature = QPixmap(":/{0}_mature.png".format(name))
+        old = QPixmap(":/{0}_old.png".format(name))
+        crop_pictures = [seed, seedling, young, mature, old]
 
-		#add the graphics to scenes
-		self.crop_scenes = []
-		for each in crop_pictures:
-			self.crop_scenes.append(QGraphicsScene())
-			self.crop_scenes[-1].addPixmap(each)
-		self.setScene(self.crop_scenes[0]) #set the initial scene
+        #add the graphics to scenes
+        self.crop_scenes = []
+        for each in crop_pictures:
+            self.crop_scenes.append(QGraphicsScene())
+            self.crop_scenes[-1].addPixmap(each)
+        self.setScene(self.crop_scenes[0]) #set the initial scene
 
-	def switch_scene(self,scene):
-		self.setScene(self.crop_scenes[scene])
+    def switch_scene(self,scene):
+        self.setScene(self.crop_scenes[scene])
 
 class WheatView(CropView):
     def __init__(self):
--- manual_grow_dialog_class.py
+++ manual_grow_dialog_class.py
@@ -1,30 +1,30 @@
 from PyQt4.QtGui import *
 
 class ManualGrowDialog(QDialog):
-	"""this class provides a dialog window to ask for light and water values"""
+    """this class provides a dialog window to ask for light and water values"""
 
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.water_line_edit = QLineEdit()
-		self.light_line_edit = QLineEdit()
+        self.water_line_edit = QLineEdit()
+        self.light_line_edit = QLineEdit()
 
-		self.water_line_edit.setPlaceholderText("Water Value")
-		self.light_line_edit.setPlaceholderText("Light Value")
+        self.water_line_edit.setPlaceholderText("Water Value")
+        self.light_line_edit.setPlaceholderText("Light Value")
 
-		self.water_line_edit.clearFocus()
+        self.water_line_edit.clearFocus()
 
-		self.submit_button = QPushButton("Enter Values")
+        self.submit_button = QPushButton("Enter Values")
 
-		self.dialog_layout = QVBoxLayout()
-		self.dialog_layout.addWidget(self.light_line_edit)
-		self.dialog_layout.addWidget(self.water_line_edit)
-		self.dialog_layout.addWidget(self.submit_button)
+        self.dialog_layout = QVBoxLayout()
+        self.dialog_layout.addWidget(self.light_line_edit)
+        self.dialog_layout.addWidget(self.water_line_edit)
+        self.dialog_layout.addWidget(self.submit_button)
 
-		self.setLayout(self.dialog_layout)
+        self.setLayout(self.dialog_layout)
 
-		#connections
-		self.submit_button.clicked.connect(self.close)
+        #connections
+        self.submit_button.clicked.connect(self.close)
 
-	def values(self):
-		return int(self.light_line_edit.text()), int(self.water_line_edit.text())
+    def values(self):
+        return int(self.light_line_edit.text()), int(self.water_line_edit.text())
