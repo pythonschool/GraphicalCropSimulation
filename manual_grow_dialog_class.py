4fe4da120eddb0b4b90099f060193430766e503e
Adam McNicol <adammcnicol@mac.com>
2013-02-24 11:29:16 +0000

    convert indentation to spaces
    Signed-off-by: Adam McNicol <adammcnicol@mac.com>

--- manual_grow_dialog_class.py
+++ manual_grow_dialog_class.py
@@ -1,34 +1,34 @@
 from PyQt4.QtGui import *
 
 class ManualGrowDialog(QDialog):
-	"""this class provides a dialog window to ask for light and water values"""
+    """this class provides a dialog window to ask for light and water values"""
 
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.water_spinbox = QSpinBox()
-		self.light_spinbox = QSpinBox()
+        self.water_spinbox = QSpinBox()
+        self.light_spinbox = QSpinBox()
 
-		self.water_spinbox.setRange(0,10)
-		self.light_spinbox.setRange(0,10)
+        self.water_spinbox.setRange(0,10)
+        self.light_spinbox.setRange(0,10)
 
-		self.water_spinbox.setSuffix(" Water")
-		self.light_spinbox.setSuffix(" Light")
+        self.water_spinbox.setSuffix(" Water")
+        self.light_spinbox.setSuffix(" Light")
 
-		self.water_spinbox.setValue(1)
-		self.light_spinbox.setValue(1)
+        self.water_spinbox.setValue(1)
+        self.light_spinbox.setValue(1)
 
-		self.submit_button = QPushButton("Enter Values")
+        self.submit_button = QPushButton("Enter Values")
 
-		self.dialog_layout = QVBoxLayout()
-		self.dialog_layout.addWidget(self.light_spinbox)
-		self.dialog_layout.addWidget(self.water_spinbox)
-		self.dialog_layout.addWidget(self.submit_button)
+        self.dialog_layout = QVBoxLayout()
+        self.dialog_layout.addWidget(self.light_spinbox)
+        self.dialog_layout.addWidget(self.water_spinbox)
+        self.dialog_layout.addWidget(self.submit_button)
 
-		self.setLayout(self.dialog_layout)
+        self.setLayout(self.dialog_layout)
 
-		#connections
-		self.submit_button.clicked.connect(self.close)
+        #connections
+        self.submit_button.clicked.connect(self.close)
 
-	def values(self):
-		return int(self.light_spinbox.value()), int(self.water_spinbox.value())
+    def values(self):
+        return int(self.light_spinbox.value()), int(self.water_spinbox.value())
